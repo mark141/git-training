65651c58a263a7072e67f62152b2d3edba5fc0b6
markus.nagel <markus.nagel@now-gmbh.de>
2024-12-09 14:09:30 +0100

    added adjustment

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-# This is a sample Python script.
+# This is a sample Python script. from markus
 
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi(first_name, last_name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+    print(f'Hi, {first_name + " " + last_name}')  # Press Strg+F8 to toggle the breakpoint.
     # really useful change
     print("nothing happens")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi(
+        'Markus',
+        'Nagel',
+    )
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
