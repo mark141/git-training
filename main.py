3b33f1f3f25d7aa74f240fe42be497d175c73fed
markus.nagel <markus.nagel@now-gmbh.de>
2024-12-09 14:14:48 +0100

    main.py improvement

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-# This is a sample Python script.
+# This is a sample Python script. And it ireally is.
 
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi(name): # such a boring function
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
     # really useful change
